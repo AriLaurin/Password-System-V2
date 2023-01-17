6692cbeb628847a1b96f37a45f504c6633a93422
AriLaurin <arijuslaurin@gmail.com>
2023-01-17 20:38:34 +0100

    Connected mongoose

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //requirements
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const serverRoutes = require("./routes/serverRoutes");
@@ -15,9 +16,10 @@ app.set("view engine", "ejs");
 
 // database connection
 const dbURI = process.env.host
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.set("strictQuery", false);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true});
+
+
 
 // Routes
 app.use(serverRoutes);
