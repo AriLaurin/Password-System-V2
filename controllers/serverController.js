30bf8745fb00310348fb1e5ab9446c39ba2589ce
AriLaurin <arijuslaurin@gmail.com>
2023-02-07 10:17:37 +0100

    Finished project

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -40,25 +40,24 @@ module.exports.forgotpass_post = (req,res) => {
       } else {
         // process result
         console.log(result);
+        let mailOptions = {
+          from: process.env.MAIL_USERNAME,
+          to: process.env.MAIL_USERNAME,
+          subject: 'LL Security - Password reset verification',
+          text: `A password reset request has been sent. If this was you, please forward yourself to this link to continue: http://localhost/newpass?email=${result._id}`
+        };
+      
+        
+      
+        transporter.sendMail(mailOptions, function(err, info) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Email sent successfully" + info.messageId);
+          }
+        });
       }
     });
-
-    let mailOptions = {
-        from: process.env.MAIL_USERNAME,
-        to: process.env.MAIL_USERNAME,
-        subject: 'LL Security - Password reset verification',
-        text: `A password reset request has been sent. If this was you, please forward yourself to this link to continue: http://localhost/newpass?email=${data}`
-      };
-    
-      
-    
-      transporter.sendMail(mailOptions, function(err, info) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Email sent successfully" + info.messageId);
-        }
-      });
 
     //console.log(data);
 }
